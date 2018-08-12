deb242be0ce8e8ea4044dd71fb4d08208a534837
Laura Del Beccaro <lauradelbeccaro@gmail.com>
2018-08-11 23:12:26 -0700

    responsive styles

--- src/components/app.js
+++ src/components/app.js
@@ -117,7 +117,7 @@ class App extends React.Component {
             <div className='nav'>
               <div className='search-bar'>
                 <svg className="search-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" data-reactid="1001"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
-                <input value={this.state.searchTerm} onChange={this.updateSearchTerm} placeholder='Search (name, address, email, phone)' />
+                <input value={this.state.searchTerm} onChange={this.updateSearchTerm} placeholder='Search people' />
               </div>
               <div className='view-selection'>
                 <div
